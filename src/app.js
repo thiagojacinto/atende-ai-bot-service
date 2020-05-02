6a811e67004d8b1e2d9567861e65e4d60427db0d
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-02 16:38:19 -0300

    [melhorias] transformou em server + implementou método POST para respostas

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const twilio = require('twilio');
+const bodyParser = require('body-parser');
 
 const ACC_SID = process.env.TWILIO_ACCOUNT_SID;
 const TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const PORT = process.env.PORT;
 
 const cliente = twilio(ACC_SID, TOKEN);
-const app = express();
+const messagingResponse = twilio.twiml.MessagingResponse;
 
-cliente.messages.create({
-  from: 'whatsapp:+14155238886',
-  to: 'whatsapp:+558197248002',
-  body: 'Olá! Deseja verificar disponibilidade em minha agenda?'
-}).then(console.log);
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (request, response) => {
+
+  cliente.messages.create({
+    from: 'whatsapp:+14155238886',
+    to: 'whatsapp:+558197248002',
+    body: 'Olá! Deseja verificar disponibilidade em minha agenda?'
+  }).then(console.log);
+});
+
+
+app.post('/resposta', (request, response) => {
+  const msgRecebida = request.body.Body;
+  const msgResposta = new messagingResponse();
+  
+  msgRecebida.message(`Certo, você digitou '${msgRecebida}'. Aguarde enquanto acerto alguns detalhes ...`);
+  response.send(msgResposta.toString());
+});
+
+
+app.listen(PORT, () => console.log(`Applicacao rodando na porta: ${PORT}`));
