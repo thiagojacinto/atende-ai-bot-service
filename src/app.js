a261142ec8b34c2b566f1b6f8ce09df8e16c474b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-02 20:32:48 -0300

    [config] more env variables

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const ACC_SID = process.env.TWILIO_ACCOUNT_SID;
 const TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const PORT = process.env.PORT;
+const TO_PHONE = process.env.TO_PHONE;
+const FROM_PHONE = process.env.FROM_PHONE;
 
 const cliente = twilio(ACC_SID, TOKEN);
 const messagingResponse = twilio.twiml.MessagingResponse;
@@ -16,8 +18,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.get('/saudar', (request, response) => {
 
   cliente.messages.create({
-    from: 'whatsapp:+14155238886',
-    to: 'whatsapp:+558197248002',
+    from: FROM_PHONE,
+    to: TO_PHONE,
     body: 'Olá! Deseja verificar disponibilidade em minha agenda?'
   }).then(console.log);
 });
