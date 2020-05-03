47cc120f501c020ef3169239e1cc9ce3798d52d2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-02 23:09:38 -0300

    [abstracao] controller para as acoes manuais, e básicas

---
+++ src/Controllers/ManualController.js
@@ -0,0 +1,26 @@
+const twilio = require('twilio');
+const cliente = twilio(config.ACC_SID, config.TOKEN);
+const messagingResponse = twilio.twiml.MessagingResponse;
+
+module.exports = {
+
+  async enviarMensagem(request, response, mensagem) {
+
+    cliente.messages.create({
+
+      from: config.FROM_PHONE,
+      to: config.TO_PHONE,
+      body: mensagem || 'Olá! Deseja verificar disponibilidade em minha agenda?'
+
+    }).then(console.log);
+  },
+
+  async responderAposProvocacao(request, response) {
+    const msgRecebida = request.body.Body;
+    const msgResposta = new messagingResponse();
+    
+    msgResposta.message(`Certo, você digitou '${msgRecebida}'. Aguarde enquanto acerto alguns detalhes ...`);
+    response.send(msgResposta.toString());
+  }
+
+}
