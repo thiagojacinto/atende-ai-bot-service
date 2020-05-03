17b416b2ed39090ad5157199171bbc38cae79018
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-02 23:10:31 -0300

    [refactor] transferindo as atividades manuais para o controller correspondente

--- src/rotas.js
+++ src/rotas.js
@@ -2,32 +2,16 @@ const { Router } = require('express');
 const config = require('./config');
 const twilio = require('twilio');
 
-
 const rotas = Router();
+const ManualController = require('./Controllers/ManualController');
 
 rotas.post('/v1/agendar', );
 rotas.get('/v1/agendamentos', ); 
 
-
 // Rotas manuais
-const cliente = twilio(config.ACC_SID, config.TOKEN);
-const messagingResponse = twilio.twiml.MessagingResponse;
 
-rotas.get('/saudar', (request, response) => {
+rotas.get('/alpha/saudar', ManualController.enviarMensagem(req, res, null));
 
-  cliente.messages.create({
-    from: config.FROM_PHONE,
-    to: config.TO_PHONE,
-    body: 'Olá! Deseja verificar disponibilidade em minha agenda?'
-  }).then(console.log);
-});
-
-rotas.post('/resposta', (request, response) => {
-  const msgRecebida = request.body.Body;
-  const msgResposta = new messagingResponse();
-  
-  msgResposta.message(`Certo, você digitou '${msgRecebida}'. Aguarde enquanto acerto alguns detalhes ...`);
-  response.send(msgResposta.toString());
-});
+rotas.post('/alpha/resposta', ManualController.responderAposProvocacao(req, res));
 
 module.exports = rotas;
