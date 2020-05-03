3b60503a106dc15a8307ccc20a3733f44ca0e888
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-03 20:37:24 -0300

    [config] Parametros da DB

--- src/config.js
+++ src/config.js
@@ -5,11 +5,17 @@ const TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const PORT = process.env.PORT;
 const TO_PHONE = process.env.TO_PHONE;
 const FROM_PHONE = process.env.FROM_PHONE;
+const DB_PWD = process.env.DB_PWD;
+const DB_USER = process.env.DB_USER;
+const DB_URL = process.env.DB_URL;
 
 module.exports = {
   ACC_SID,
   TOKEN,
   PORT,
   TO_PHONE,
-  FROM_PHONE
+  FROM_PHONE,
+  DB_PWD,
+  DB_USER,
+  DB_URL
 }
