d0dc1d29936b5584aa0d139b4d8e4cdefcb6c150
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-02 23:00:39 -0300

    [abstracao] separação em: configuracao, rotas, controllers

--- src/app.js
+++ src/app.js
@@ -1,36 +1,12 @@
-const dotenv = require('dotenv').config();
 const express = require('express');
-const twilio = require('twilio');
 const bodyParser = require('body-parser');
 
-const ACC_SID = process.env.TWILIO_ACCOUNT_SID;
-const TOKEN = process.env.TWILIO_AUTH_TOKEN;
-const PORT = process.env.PORT;
-const TO_PHONE = process.env.TO_PHONE;
-const FROM_PHONE = process.env.FROM_PHONE;
-
-const cliente = twilio(ACC_SID, TOKEN);
-const messagingResponse = twilio.twiml.MessagingResponse;
+const { PORT } = require('./config');
+const { rotas } = require('./rotas');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:false}));
-
-app.get('/saudar', (request, response) => {
-
-  cliente.messages.create({
-    from: FROM_PHONE,
-    to: TO_PHONE,
-    body: 'Olá! Deseja verificar disponibilidade em minha agenda?'
-  }).then(console.log);
-});
-
-app.post('/resposta', (request, response) => {
-  const msgRecebida = request.body.Body;
-  const msgResposta = new messagingResponse();
-  
-  msgResposta.message(`Certo, você digitou '${msgRecebida}'. Aguarde enquanto acerto alguns detalhes ...`);
-  response.send(msgResposta.toString());
-});
-
+app.use(rotas);
 
 app.listen(PORT, () => console.log(`Applicacao rodando na porta: ${PORT}`));
