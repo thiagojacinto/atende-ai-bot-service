bbc0d10622ab939c04479c72d8f81ab3f1be0442
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-03 20:39:23 -0300

    [config] conexão db + req.body usando `bodyParser.json()`

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./config');
-const { rotas } = require('./rotas');
+const { PORT, DB_URL } = require('./config');
+const rotas = require('./rotas');
 
 const app = express();
 
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(rotas);
 
