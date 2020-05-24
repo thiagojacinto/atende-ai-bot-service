10f0cc62beba0f17fabe8be378cf2921e4318ff8
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-23 22:39:15 -0300

    [novo] format + rota para remocao

--- src/rotas.js
+++ src/rotas.js
@@ -1,20 +1,20 @@
-const { Router } = require('express');
-const config = require('./config');
-const twilio = require('twilio');
+const { Router } = require("express");
+const config = require("./config");
+const twilio = require("twilio");
 
 const rotas = Router();
-const ManualController = require('./Controllers/ManualController');
-const AgendaController = require('./Controllers/AgendaController');
+const ManualController = require("./Controllers/ManualController");
+const AgendaController = require("./Controllers/AgendaController");
 
-rotas.post('/v1/agendar', AgendaController.store);
-rotas.get('/v1/agendamentos', AgendaController.index);
-rotas.post('/v1/agendamento', AgendaController.show);
-rotas.post('/v1/confirmacao', AgendaController.toggleConfirmation); 
+rotas.post("/v1/agendar", AgendaController.store);
+rotas.get("/v1/agendamentos", AgendaController.index);
+rotas.post("/v1/agendamento", AgendaController.show);
+rotas.delete("/v1/agendamento", AgendaController.destroy);
+rotas.post("/v1/confirmacao", AgendaController.toggleConfirmation);
 
 // Rotas manuais
 
-rotas.get('/alpha/saudar', ManualController.enviarMensagem);
-
-rotas.post('/alpha/resposta', ManualController.responderAposProvocacao);
+rotas.get("/alpha/saudar", ManualController.enviarMensagem);
+rotas.post("/alpha/resposta", ManualController.responderAposProvocacao);
 
 module.exports = rotas;
