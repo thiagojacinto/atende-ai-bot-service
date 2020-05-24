c4daa643c060de5242345d5a261b3eaf1db24543
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-05-23 22:19:16 -0300

    [mudanca] agendamento verifica duplicidade por numero + formatting

--- src/Controllers/AgendaController.js
+++ src/Controllers/AgendaController.js
@@ -1,8 +1,7 @@
 // const axios = require('axios');
-const Agendamento = require('../Models/Agendamento');
+const Agendamento = require("../Models/Agendamento");
 
 module.exports = {
-
   async index(request, response) {
     const todosAgendamentos = await Agendamento.find();
     return response.json(todosAgendamentos);
@@ -12,44 +11,67 @@ module.exports = {
     console.log(`Body element: ${request.body}`); // DEBUG
     const { telefone, observacoes, confirmacao, maps } = request.body;
 
-    let data = new Date();
+    await Agendamento.findOne(
+      { telefone: telefone },
+      async (err, duplicata) => {
+        if (err) console.error(err);
+        if (duplicata) {
+          console.log(`Telefone ${telefone} já contem registro.`);
+          return response.json({});
+        }
 
-    let agend = await Agendamento.create({
-      telefone,
-      data,
-      observacoes,
-      confirmacao,
-      maps
-    })
-      .then(console.log('Agendamento realizado com sucesso.'))
-      .catch(console.error);
-      
-    return response.json(agend);
+        let data = new Date();
+        let agend = await Agendamento.create({
+          telefone,
+          data,
+          observacoes,
+          confirmacao,
+          maps,
+        })
+          .then(console.log("Agendamento realizado com sucesso."))
+          .catch(console.error);
+
+        return response.json(agend);
+      }
+    );
   },
 
   async show(request, response) {
     const { telefone } = request.body;
 
-    let result = await Agendamento.findOne(
-      { "telefone": telefone },
-    ).then(response.status(200))
-    .catch(console.error);
+    let result = await Agendamento.findOne({ telefone: telefone })
+      .then(response.status(200))
+      .catch(console.error);
 
     return response.json(result);
   },
 
   async toggleConfirmation(request, response) {
     const { telefone } = request.body;
-    let agendamento = await Agendamento.findOne({ "telefone": telefone })
-      .catch(console.error);
-    
+    let agendamento = await Agendamento.findOne({ telefone: telefone }).catch(
+      console.error
+    );
+
     // updating information:
     agendamento.confirmacao = !agendamento.confirmacao;
-    agendamento.observacoes = agendamento.observacoes.concat(`, ${agendamento.confirmacao ? 'confirmado' : 'desmarcado' } em ${new Date().toLocaleDateString('pt-BR')}`);
-    await agendamento.save()
-      .then(response.status(200).send(`Agendamento ${agendamento.confirmacao ? 'confirmado' : 'desmarcado'}.`))
+    agendamento.observacoes = agendamento.observacoes.concat(
+      `, ${
+        agendamento.confirmacao ? "confirmado" : "desmarcado"
+      } em ${new Date().toLocaleDateString("pt-BR")}`
+    );
+    await agendamento
+      .save()
+      .then(
+        response
+          .status(200)
+          .send(
+            `Agendamento ${
+              agendamento.confirmacao ? "confirmado" : "desmarcado"
+            }.`
+          )
+      )
       .catch(console.error);
-      
+
     return response;
-  }
-}
+  },
+};
